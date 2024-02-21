3a479e06dfb8b68e1f0abe727e6e929dc69fb299
MAGNIFI88 <sergiovargasventura@yahoo.es>
2024-02-21 20:26:52 +0000

    Setting up react icons

--- chessPal/frontend/src/components/Navbar.js
+++ chessPal/frontend/src/components/Navbar.js
@@ -1,11 +1,28 @@
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import { FiMenu, FiX } from "react-icons/fi";
+import { FaChessKnight } from "react-icons/fa";
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
   return (
     <>
       <nav className="navbar">
-        <div className="navbar-container">a</div>
+        <div className="navbar-container">
+          {/* Homepage */}
+          <Link to="/" className="navbar-logo">
+            ChessPal
+            <FaChessKnight className="navbar-logo-icon" />
+          </Link>
+
+          {/* Interaction menu */}
+          <div className="navbar-menu" onClick={handleClick}>
+            {click ? <FiX /> : <FiMenu />}
+          </div>
+        </div>
       </nav>
     </>
   );
