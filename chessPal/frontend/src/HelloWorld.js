4694d61e6d877003d22c87d50b6042b761162ca7
Charlotte Moremen <charlottebmoremen@gmail.com>
2024-02-22 09:14:38 +0000

    rudimentary landing page

--- chessPal/frontend/src/HelloWorld.js
+++ chessPal/frontend/src/HelloWorld.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+
 function HelloWorld() {
   const [message, setMessage] = useState("");
 
---
+++ chessPal/frontend/src/Images/logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import chessPalLogo from './ChessPalLogo.png';
+
+const logo = (
+    <img
+    src = {chessPalLogo}
+    alt = "Chess Pal Logo"
+    />
+);
+
