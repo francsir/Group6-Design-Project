30d6698192def1deaae6a3e2c1ebce2306b69bf2
MAGNIFI88 <sergiovargasventura@yahoo.es>
2024-02-23 12:48:24 +0000

    Added logo

--- chessPal/frontend/src/components/Navbar.js
+++ chessPal/frontend/src/components/Navbar.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FaChessKnight } from "react-icons/fa";
 import "./styles/Navbar.css";
 import Button from "@mui/material/Button";
+import logo from "../images/ChessPalLogoTransparent.png";
 
 function Navbar() {
   return (
     <header>
       <h3>
         <Link to="/">
-          ChessPal
-          <FaChessKnight />
+          <img src={logo} alt="ChessPal Logo" className="logo-image" />
         </Link>
       </h3>
       <nav>
@@ -18,8 +17,16 @@ function Navbar() {
         <Link to="/historial">Historial</Link>
         <Link to="/discover">Discover</Link>
         <div className="buttons">
-          <Button variant="contained">log in</Button>
-          <Button variant="contained">sign up</Button>
+          <Link to="/login">
+            <Button variant="outlined" sx={{ borderRadius: "20px" }}>
+              log in
+            </Button>
+          </Link>
+          <Link to="/signup">
+            <Button variant="contained" sx={{ borderRadius: "20px" }}>
+              sign up
+            </Button>
+          </Link>
         </div>
       </nav>
     </header>
