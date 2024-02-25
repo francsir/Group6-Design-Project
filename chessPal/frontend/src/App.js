9f5cb17e42c644d1105483aab1559c1971124438
MAGNIFI88 <sergiovargasventura@yahoo.es>
2024-02-25 23:47:28 +0000

    First version of Navbar and Homepage

--- chessPal/frontend/src/App.js
+++ chessPal/frontend/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import "./App.css";
+import "./styles/App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; //Routes is the updated Switch
+import Homepage from "./sections/Homepage";
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
+        <Homepage />
         <Routes>
           <Route path="/" exact />
         </Routes>
--- chessPal/frontend/src/components/Navbar.js
+++ chessPal/frontend/src/components/Navbar.js
@@ -1,36 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./styles/Navbar.css";
-import Button from "@mui/material/Button";
+import { FaBars, FaTimes } from "react-icons/fa";
 import logo from "../images/ChessPalLogoTransparent.png";
+import Button from "@mui/material/Button";
 
-function Navbar() {
+import "../styles/Navbar.css";
+
+const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
+  const closeMenu = () => setClick(false);
+
   return (
-    <header>
-      <h3>
-        <Link to="/">
-          <img src={logo} alt="ChessPal Logo" className="logo-image" />
-        </Link>
-      </h3>
-      <nav>
-        <Link to="/scan">Scan</Link>
-        <Link to="/historial">Historial</Link>
-        <Link to="/discover">Discover</Link>
-        <div className="buttons">
-          <Link to="/login">
-            <Button variant="outlined" sx={{ borderRadius: "20px" }}>
-              log in
-            </Button>
-          </Link>
-          <Link to="/signup">
-            <Button variant="contained" sx={{ borderRadius: "20px" }}>
-              sign up
-            </Button>
-          </Link>
+    <div className="header">
+      <nav className="navbar">
+        <a href="/" className="logo">
+          <img src={logo} alt="logo" />
+        </a>
+        <div className="menu-icon" onClick={handleClick}>
+          {click ? (
+            <FaTimes size={30} style={{ color: "#000000" }} />
+          ) : (
+            <FaBars size={30} style={{ color: "#000000" }} />
+          )}
         </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/scan" onClick={closeMenu}>
+              SCAN
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/historial" onClick={closeMenu}>
+              HISTORIAL
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/discover" onClick={closeMenu}>
+              DISCOVER
+            </Link>
+          </li>
+
+          <div className="buttons">
+            <Link to="/login">
+              <Button variant="outlined" sx={{ borderRadius: "20px" }}>
+                log in
+              </Button>
+            </Link>
+            <Link to="/signup">
+              <Button variant="contained" sx={{ borderRadius: "20px" }}>
+                sign up
+              </Button>
+            </Link>
+          </div>
+        </ul>
       </nav>
-    </header>
+    </div>
   );
-}
+};
 
 export default Navbar;
