8cebdd28326ac1acdf8b0e8f22cbc77289c76841
MAGNIFI88 <sergiovargasventura@yahoo.es>
2024-03-21 16:55:52 +0000

    Backend output now displays correctly

--- chessPal/frontend/src/sections/ScanResult.js
+++ chessPal/frontend/src/sections/ScanResult.js
@@ -7,11 +7,12 @@ import Navbar from "../Components/Navbar";
 
 const ScanResult = () => {
   const location = useLocation();
-  const responseDataAAA = location.state?.responseData;
-  const responseData = `[[['1']], [['e4']], [['e5']], [['2']], [['Nh5']], [['Nc6']], [['3']], [['a6']], [['Nx6']], [['4']], [['Rh7']], [['Rh7']], [['5']], [['Rh7']], [['Rh7']], [['6']], [['Rh7']], [['Rh7']], [['7']], [['Rh7']], [['Rh7']], [['8']], [['Rh7']], [['Rh7']], [['e4']], [['Rh2']], [['Rh2']], [['1o']], [['Rh7']], [['Rh7']], [['Nh4']], [['Rh7']], [['Rh7']], [['12']], [['Rh7']], [['Rh7']], [['g5+']], [['Rh7']], [['Rh7']], [['14']], [['Rh7']], [['Rh7']], [['Ba6']], [['Rh7']], [['Rh7']], [['Nh7']], [['Rh7']], [['Rh7']], [['18']], [['Rh7']], [['Rh7']], [['Rf1']], [['Rh7']], [['Rh7']], [['2o']], [['Rh7']], [['Rh7']], [['2a']], [['Rh7']], [['Rh7']], [['fx+']], [['Rh7']], [['Rh7']], [['23']], [['Rh7']], [['Rh7']], [['24']], [['Rh7']], [['Rh7']], [['25']], [['Rh7']], [['Rh7']], [['27']], [['Rh7']], [['Rh7']], [['28']], [['Rh7']], [['Rh7']], [['f4+']], [['Rh7']], [['Rh7']], [['3o']], [['Rh7']], [['Rh7']], [['hx+']], [['Rh7']], [['Rh7']], [['32']], [['Rh7']], [['Rh7']], [['33']], [['Rh7']], [['Rh7']], [['34']], [['Rh2']], [['Rh2']], [['h4']], [['Rh7']], [['Rh7']], [['36']], [['Rh7']], [['Rh7']], [['37']], [['Rh7']], [['Rh7']], [['h4+']], [['Rh7']], [['Rh7']], [['h4+']], [['Rh7']], [['Rh7']], [['4o']], [['Rh7']], [['Rh7']], [['4']], [['Rh7']], [['Rh7']], [['42']], [['Rh7']], [['Rh7']], [['43']], [['Rh7']], [['Rh7']], [['4']], [['Rh7']], [['Rh7']], [['Kg6']], [['Rh7']], [['Rh7']], [['46']], [['Rh7']], [['Rh7']], [['48']], [['Rh7']], [['Rh7']], [['ba7+']], [['Rh7']], [['Rh7']], [['Qh1']], [['Rh7']], [['Rh7']], [['Bxe5']], [['Bg5']], [['x5']], [['Nge6+']], [['Rg7']], [['R74']], [['Nxe2']], [['Ng4']], [['fx4']]]`;
+  const responseData = location.state?.responseData;
+  const formatedresponseData = JSON.stringify(responseData, null, 2);
+  // const responseDataSample = `[[['1']], [['e4']], [['e5']], [['2']], [['Nh5']], [['Nc6']], [['3']], [['a6']], [['Nx6']], [['4']], [['Rh7']], [['Rh7']], [['5']], [['Rh7']], [['Rh7']], [['6']], [['Rh7']], [['Rh7']], [['7']], [['Rh7']], [['Rh7']], [['8']], [['Rh7']], [['Rh7']], [['e4']], [['Rh2']], [['Rh2']], [['1o']], [['Rh7']], [['Rh7']], [['Nh4']], [['Rh7']], [['Rh7']], [['12']], [['Rh7']], [['Rh7']], [['g5+']], [['Rh7']], [['Rh7']], [['14']], [['Rh7']], [['Rh7']], [['Ba6']], [['Rh7']], [['Rh7']], [['Nh7']], [['Rh7']], [['Rh7']], [['18']], [['Rh7']], [['Rh7']], [['Rf1']], [['Rh7']], [['Rh7']], [['2o']], [['Rh7']], [['Rh7']], [['2a']], [['Rh7']], [['Rh7']], [['fx+']], [['Rh7']], [['Rh7']], [['23']], [['Rh7']], [['Rh7']], [['24']], [['Rh7']], [['Rh7']], [['25']], [['Rh7']], [['Rh7']], [['27']], [['Rh7']], [['Rh7']], [['28']], [['Rh7']], [['Rh7']], [['f4+']], [['Rh7']], [['Rh7']], [['3o']], [['Rh7']], [['Rh7']], [['hx+']], [['Rh7']], [['Rh7']], [['32']], [['Rh7']], [['Rh7']], [['33']], [['Rh7']], [['Rh7']], [['34']], [['Rh2']], [['Rh2']], [['h4']], [['Rh7']], [['Rh7']], [['36']], [['Rh7']], [['Rh7']], [['37']], [['Rh7']], [['Rh7']], [['h4+']], [['Rh7']], [['Rh7']], [['h4+']], [['Rh7']], [['Rh7']], [['4o']], [['Rh7']], [['Rh7']], [['4']], [['Rh7']], [['Rh7']], [['42']], [['Rh7']], [['Rh7']], [['43']], [['Rh7']], [['Rh7']], [['4']], [['Rh7']], [['Rh7']], [['Kg6']], [['Rh7']], [['Rh7']], [['46']], [['Rh7']], [['Rh7']], [['48']], [['Rh7']], [['Rh7']], [['ba7+']], [['Rh7']], [['Rh7']], [['Qh1']], [['Rh7']], [['Rh7']], [['Bxe5']], [['Bg5']], [['x5']], [['Nge6+']], [['Rg7']], [['R74']], [['Nxe2']], [['Ng4']], [['fx4']]]`;
 
-  const [title, setTitle] = useState("exampleTitle(max256chars)");
-  const [content, setContent] = useState(responseData);
+  const [title, setTitle] = useState("Example Title");
+  const [content, setContent] = useState(formatedresponseData);
 
   // Function to handle download button click
   const handleDownload = () => {
@@ -34,7 +35,6 @@ const ScanResult = () => {
         <h1>Your file is ready!</h1>
         <p>Name your file and make sure there are no errors</p>
         <div className="output-container">
-          <div className="output-tabs">tabs</div>
           <div className="output-main">
             <p>Title</p>
             <input
@@ -43,28 +43,28 @@ const ScanResult = () => {
               value={title}
               onChange={(e) => setTitle(e.target.value)}
             />
-            <p>PGN</p>
+            <p>Edit</p>
             <textarea
-              className="PGN"
+              className="edit"
               value={content}
               onChange={(e) => setContent(e.target.value)}
             />
-          </div>
-        </div>
 
-        <div className="buttons">
-          <Link to="/homepage">
-            <Button variant="contained" sx={{ borderRadius: "20px" }}>
-              Homepage
-            </Button>
-          </Link>
-          <Button
-            variant="contained"
-            sx={{ borderRadius: "20px" }}
-            onClick={handleDownload}
-          >
-            Download
-          </Button>
+            <div className="buttons">
+              <Link to="/homepage">
+                <Button variant="contained" sx={{ borderRadius: "20px" }}>
+                  Homepage
+                </Button>
+              </Link>
+              <Button
+                variant="contained"
+                sx={{ borderRadius: "20px" }}
+                onClick={handleDownload}
+              >
+                Download
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
     </>
