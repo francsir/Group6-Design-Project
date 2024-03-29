3ca6b15f9551e5cb5859818c29894754e261b72a
MAGNIFI88 <sergiovargasventura@yahoo.es>
2024-03-29 03:13:13 +0000

    display image with zoom functionality

--- chessPal/frontend/src/components/ScanResult.js
+++ chessPal/frontend/src/components/ScanResult.js
@@ -14,6 +14,7 @@ const ScanResult = () => {
   const { file } = location.state;
   const [showDisplay, setShowDisplay] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [scale, setScale] = useState(1);
 
   // Just for testing
   // const scanResultSample = `[[['1']], [['e4']], [['e5']], [['2']], [['Nh5']], [['Nc6']], [['3']], [['a6']], [['Nx6']], [['4']], [['Rh7']], [['Rh7']], [['5']], [['Rh7']], [['Rh7']], [['6']], [['Rh7']], [['Rh7']], [['7']], [['Rh7']], [['Rh7']], [['8']], [['Rh7']], [['Rh7']], [['e4']], [['Rh2']], [['Rh2']], [['1o']], [['Rh7']], [['Rh7']], [['Nh4']], [['Rh7']], [['Rh7']], [['12']], [['Rh7']], [['Rh7']], [['g5+']], [['Rh7']], [['Rh7']], [['14']], [['Rh7']], [['Rh7']], [['Ba6']], [['Rh7']], [['Rh7']], [['Nh7']], [['Rh7']], [['Rh7']], [['18']], [['Rh7']], [['Rh7']], [['Rf1']], [['Rh7']], [['Rh7']], [['2o']], [['Rh7']], [['Rh7']], [['2a']], [['Rh7']], [['Rh7']], [['fx+']], [['Rh7']], [['Rh7']], [['23']], [['Rh7']], [['Rh7']], [['24']], [['Rh7']], [['Rh7']], [['25']], [['Rh7']], [['Rh7']], [['27']], [['Rh7']], [['Rh7']], [['28']], [['Rh7']], [['Rh7']], [['f4+']], [['Rh7']], [['Rh7']], [['3o']], [['Rh7']], [['Rh7']], [['hx+']], [['Rh7']], [['Rh7']], [['32']], [['Rh7']], [['Rh7']], [['33']], [['Rh7']], [['Rh7']], [['34']], [['Rh2']], [['Rh2']], [['h4']], [['Rh7']], [['Rh7']], [['36']], [['Rh7']], [['Rh7']], [['37']], [['Rh7']], [['Rh7']], [['h4+']], [['Rh7']], [['Rh7']], [['h4+']], [['Rh7']], [['Rh7']], [['4o']], [['Rh7']], [['Rh7']], [['4']], [['Rh7']], [['Rh7']], [['42']], [['Rh7']], [['Rh7']], [['43']], [['Rh7']], [['Rh7']], [['4']], [['Rh7']], [['Rh7']], [['Kg6']], [['Rh7']], [['Rh7']], [['46']], [['Rh7']], [['Rh7']], [['48']], [['Rh7']], [['Rh7']], [['ba7+']], [['Rh7']], [['Rh7']], [['Qh1']], [['Rh7']], [['Rh7']], [['Bxe5']], [['Bg5']], [['x5']], [['Nge6+']], [['Rg7']], [['R74']], [['Nxe2']], [['Ng4']], [['fx4']]]`;
@@ -78,6 +79,24 @@ const ScanResult = () => {
     document.body.removeChild(a);
   };
 
+  const handleWheel = (e) => {
+    e.preventDefault();
+    const scaleAmount = 0.1;
+    const minScale = 0.5;
+    const maxScale = 3.0;
+    let newScale;
+
+    if (e.deltaY < 0) {
+      newScale = scale + scaleAmount;
+    } else {
+      newScale = scale - scaleAmount;
+    }
+
+    newScale = Math.min(Math.max(newScale, minScale), maxScale);
+
+    setScale(newScale);
+  };
+
   return (
     <>
       <Navbar />
@@ -85,7 +104,18 @@ const ScanResult = () => {
         {!showDisplay && (
           <div className={"flex-column " + styles.main1}>
             <h1>Do you want to scan this image?</h1>
-            <img src={URL.createObjectURL(file)} alt={file.name} />
+            <div className={styles.image_container}>
+              <img
+                src={URL.createObjectURL(file)}
+                alt={file.name}
+                onWheel={handleWheel}
+                style={{
+                  transform: `scale(${scale})`,
+                  transition: "transform 0.2s",
+                }}
+              />
+            </div>
+
             <div className={"flex-row " + styles.buttons}>
               <Link to="/homepage">
                 <Button
