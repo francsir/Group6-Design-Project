e166c8479a535c2c2f2646a5be3aecf6165863ee
MAGNIFI88 <sergiovargasventura@yahoo.es>
2024-03-30 17:36:04 +0000

    Setting up files 2

---
+++ chessPal/frontend/src/components/LogInPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LogInPage() {
+  return (
+    <>
+      <div>a</div>
+    </>
+  );
+}
+
+export default LogInPage;
---
+++ chessPal/frontend/src/components/SignUpPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SignUpPage() {
+  return (
+    <>
+      <div>a</div>
+    </>
+  );
+}
+
+export default SignUpPage;
